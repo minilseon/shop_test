6bcacec69030d5aaedba24d13c8a6d05cdd6f504
Min-Ilseon <99113480+minilseon@users.noreply.github.com>
2025-08-06 12:26:34 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/shop_test'>
+      <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
         <App />
       </BrowserRouter>
     </Provider>
